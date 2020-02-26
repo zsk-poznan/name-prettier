d8b72ba3dac18ba217ec16fc108bce888520f568
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-26 10:30:00 +0100

    Version 1.1

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="name_prettier",
-    version="0.1",
+    version="1.1",
     packages=find_packages(),
     package_data={
         "": ["*.json"]
